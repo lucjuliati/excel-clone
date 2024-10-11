b17b7a8ec315c1c6a1ddf1d6669e37655f6ccae6
hppsrc <horahenaripo@gmail.com>
2024-10-11 16:17:01 -0500

    added tollbar, kinda working

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 
+
 function generateSpreadsheet() {
     const spreadsheet = document.querySelector(".spreadsheet")
     let matrix = []
@@ -21,5 +22,46 @@ function generateSpreadsheet() {
         })
     })
 }
+
+let button = document.getElementsByTagName('button');
+let menuVar = document.getElementById('menuShow');
+
+
+fileMenu = [
+    {name:"New", disable:"true", action: "alert()"},
+    {name:"Open", disable:"true", action: "alert()"},
+    {name:"Save", disable:"true", action: "alert()"}
+]
+
+menus = {
+    fileMenu: fileMenu,
+    editMenu: editMenu,
+    helpMenu: helpMenu
+}
+function getOffset(el) {
+    const rect = el.getBoundingClientRect();
+    return rect.left + window.scrollX;
+}
+
+Array.from(button).forEach(e => e.addEventListener('click', () => {
+    menuVar.style.display = 'block';
+    menuVar.style.left = getOffset(e)+"px";
+
+    let current = e.textContent.toLocaleLowerCase()+"Menu";
+
+    let values = menus[current];
+
+    menuVar.innerHTML = "";
+
+    for (let i = 0; i < menus[current].length; i++) {
+        console.log(values[i].name);
+        var div = document.createElement('div');
+        div.innerText = values[i].name;
+        menuVar.appendChild(div);
+        
+    }
+    
+    
+}));
 
 generateSpreadsheet()
